182517c11b24654cf42cce8629f431b4e68799e3
Takashi-Matsumura <mats@coral.plala.or.jp>
2024-07-02 15:24:22 +0900

    7th commit

--- app/api/user/route.ts
+++ app/api/user/route.ts
@@ -3,6 +3,8 @@ import { NextResponse } from "next/server";
 
 const prisma = new PrismaClient();
 
+export const dynamic = 'force-dynamic'
+
 export const GET = async (req: Request, res: NextResponse) => {
     const users = await prisma.user.findMany({
       orderBy: {
